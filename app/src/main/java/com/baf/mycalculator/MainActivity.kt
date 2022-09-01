795477b8b7f443e1b5dbd1e1c3b5ed97180563fa
Ilias Baferos <ilias.baferos@sonova.com>
2022-09-01 10:50:35 +0200

    Layout ready

--- app/src/main/java/com/baf/mycalculator/MainActivity.kt
+++ app/src/main/java/com/baf/mycalculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.baf.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        Toast.makeText(this, "Digit clicked", Toast.LENGTH_SHORT).show()
+
+
     }
 }
